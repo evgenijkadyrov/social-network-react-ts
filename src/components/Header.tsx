0ec979eed620aa4189c5026f1f6c2af1c14df409
Yauheni Kadyrau <7077634@gmail.com>
2022-01-27 14:25:40 +0300

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Header = ()=> {
+    return     <header className='header'>
+        <img
+            src='http://www.logobook.com/wp-content/uploads/2019/02/u_Bear_logo-1.svg'/>
+    </header>
+}
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img
+                src='https://cdn.pixabay.com/photo/2018/11/29/21/51/social-media-3846597_960_720.png'/>
+
+        </div>
+        <div>
+            ava+desc
+        </div>
+        <div>
+            My posts
+
+            <div>New Post</div>
+            <div>
+                <div>post1</div>
+                <div>post2</div>
+                <div>post3</div>
+            </div>
+
+        </div>
+    </div>
+}
+export default Profile;
