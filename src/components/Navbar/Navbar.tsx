455db0c6babdbaf4202403938b3b402dc777eb83
Yauheni Kadyrau <7077634@gmail.com>
2022-04-12 22:44:30 +0300

    53 lesson

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,6 +9,7 @@ type NavbarPropsType={
     return    <nav className={s.nav}>
         <div className={s.item}><Link to= {'profile'} >Profile</Link> </div>
         <div className={s.item}><Link to={'dialogs'}>Message</Link></div>
+        <div className={s.item}><Link to={'users'}>Users</Link></div>
         <div className={s.item}><Link  to={'news'}>News</Link></div>
         <div className={s.item}><Link  to={'music'}>Music</Link></div>
         <div className={s.item}><Link  to={'settings'}>Settings</Link></div>
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,34 +2,37 @@ import React from "react";
 import styles from './Users.module.css'
 import {UsersPropsType} from "./UsersContainer";
 import axios from "axios";
+import {AppStateType} from "../../redux/redux-store";
 
 
+export class Users extends React.Component<UsersPropsType,AppStateType  > {
+    constructor(props:UsersPropsType) {
+        super(props);
 
-export const Users = (props: UsersPropsType) => {
-    if (props.usersPage.users.length === 0) {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response=>{
-
-                props.setUsers(response.data.items)
+            .then(response => {
+                this.props.setUsers(response.data.items)
             })
-
     }
-const avatar=require('../../common/avatars/user.png')
-    return (
-        <div>
+
+    avatar = require('../../common/avatars/user.png')
+
+    render() {
+        return <div>
             {
-                props.usersPage.users.map(ul => <div key={ul.id}>
+                this.props.usersPage.users.map(ul => <div key={ul.id}>
                     <span>
                         <div>
-                            <img src={ul.photos.small!==null? ul.photos.small:String(avatar)} className={styles.userPhoto}/>
+                            <img src={ul.photos.small !== null ? ul.photos.small : String(this.avatar)}
+                                 className={styles.userPhoto}/>
                         </div>
                         <div>
                             {ul.followed
                                 ? <button onClick={() => {
-                                    props.unfollow(ul.id)
+                                    this.props.unfollow(ul.id)
                                 }}>UnFollow</button>
                                 : <button onClick={() => {
-                                    props.follow(ul.id)
+                                    this.props.follow(ul.id)
                                 }}>Follow</button>}
 
                         </div>
@@ -43,5 +46,7 @@ const avatar=require('../../common/avatars/user.png')
                 </div>)
             }
         </div>
-    )
+    }
 }
+
+
