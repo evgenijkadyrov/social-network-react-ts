ca5dfb973d92e3b3a9d07fb427afe5d63f1d587f
Yauheni Kadyrau <7077634@gmail.com>
2022-04-12 21:06:50 +0300

    51 lesson

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -10,7 +10,6 @@ export const Users = (props: UsersPropsType) => {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response=>{
 
-
                 props.setUsers(response.data.items)
             })
 
